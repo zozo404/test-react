80636b9e2109e673f3d2c2ae7943d85b36d2cc9e
Enzo <enzocuny66@gmail.com>
2022-06-13 15:43:31 +0200

    :sparkles: | ajout bar nmbr de pays

--- src/compenents/Countries.js
+++ src/compenents/Countries.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import Card from './Card';
 
 const Countries = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [rangeValue, setRangeValue] = useState(36);
 
 
     // Le useEffect se joue lorsque le composant est monté
@@ -15,10 +15,13 @@ const Countries = () => {
 
     return (
         <div className='countries'>
-            <h1>Countries</h1>
+            <ul className="radio-container">
+                <input type="range" min="1" max="250" defaultValue={rangeValue} onChange={(e)=> setRangeValue(e.target.value)}/>
+            </ul>
             <ul>
                 {
-                    data.map((country, index)=> (
+                    data
+                    .slice(0,rangeValue).map((country, index)=> (
                         <Card key={index} country={country} ></Card>
                     ))
                 }
